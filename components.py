f900826a21da054aee938feb3180295bfa628e68
Paul Bricman <paulbricman@protonmail.com>
2022-01-07 13:54:22 +0200

    feat: comboed elems can be shuffled

--- components.py
+++ components.py
@@ -6,20 +6,26 @@ import pandas as pd
 
 kb = json.load(open('kb.json', 'r'))
 
+
 def hero_section():
     st.markdown('# 🤸 gymnastics expert system')
     st.markdown('')
-    st.info('This project has been implemented in the context of [Knowledge Technology Practical](https://www.rug.nl/ocasys/fwn/vak/show?code=WBAI014-05).')
+    st.info(
+        'This project has been implemented in the context of [Knowledge Technology Practical](https://www.rug.nl/ocasys/fwn/vak/show?code=WBAI014-05).')
     st.markdown('---')
 
 
 def notes_section(parent):
     parent.markdown('### notes')
-    st.session_state['small_mistakes'] = parent.number_input('small mistakes', step=1, value=st.session_state['small_mistakes'])
-    st.session_state['big_mistakes'] = parent.number_input('big mistakes', step=1, value=st.session_state['big_mistakes'])
-    st.session_state['falls'] = parent.number_input('falls', step=1, value=st.session_state['falls'])
-    st.session_state['connections'] = parent.number_input('connections', step=1, value=st.session_state['connections'])
-    
+    st.session_state['small_mistakes'] = parent.number_input(
+        'small mistakes', step=1, value=st.session_state['small_mistakes'])
+    st.session_state['big_mistakes'] = parent.number_input(
+        'big mistakes', step=1, value=st.session_state['big_mistakes'])
+    st.session_state['falls'] = parent.number_input(
+        'falls', step=1, value=st.session_state['falls'])
+    st.session_state['connections'] = parent.number_input(
+        'connections', step=1, value=st.session_state['connections'])
+
 
 def choose_elements():
     elements = kb['apparatuses']['beam']['elements']
@@ -30,7 +36,8 @@ def choose_elements():
     dismounts = filter_dict(elements, lambda x: ' dismount' in x[0])
 
     col1, col2, col3 = st.columns([1, 1, 1])
-    selected_mount = col1.radio('What mount element is the gymnast performing?', mounts)
+    selected_mount = col1.radio(
+        'What mount element is the gymnast performing?', mounts)
 
     col1.caption('What jump elements is the gymnast performing?')
     selected_jumps = []
@@ -50,15 +57,19 @@ def choose_elements():
         if col2.checkbox(acro):
             selected_acros += [acro]
 
-    selected_dismount = col2.radio('What dismount is the gymnast performing?', dismounts)
+    selected_dismount = col2.radio(
+        'What dismount is the gymnast performing?', dismounts)
 
-    st.session_state['selected_elements'] = [selected_mount] + selected_jumps + selected_dances + selected_acros + [selected_dismount]
-    st.session_state['selected_elements'] = [(e, elements[e]) for e in st.session_state['selected_elements']]
+    st.session_state['selected_elements'] = [selected_mount] + \
+        selected_jumps + selected_dances + selected_acros + [selected_dismount]
+    st.session_state['selected_elements'] = [
+        (e, elements[e]) for e in st.session_state['selected_elements']]
 
     notes_section(col3)
 
-    missed_elements = 6 - (2 + len(selected_jumps) + len(selected_dances) + len(selected_acros))
-    
+    missed_elements = 6 - (2 + len(selected_jumps) +
+                           len(selected_dances) + len(selected_acros))
+
     st.markdown('---')
     if st.button('Next'):
         st.session_state['state'] = 'element_walkthrough'
@@ -94,7 +105,7 @@ def detail_element(element):
 def apparatus_mistakes():
     if 'execution' not in st.session_state.keys():
         st.session_state['execution'] = 0
-        
+
     mistakes = kb['apparatuses']['beam']['mistakes']
     st.subheader('apparatus mistakes')
     for question in mistakes:
@@ -113,7 +124,7 @@ def apparatus_mistakes():
 def general_execution_mistakes():
     st.subheader('general execution mistakes')
     mistakes = kb['general_execution_mistakes']
-    
+
     for mistake in mistakes.items():
         option = st.radio(mistake[0], ['none'] + mistake[1])
         if option == 'small':
@@ -131,7 +142,7 @@ def general_execution_mistakes():
 def general_landing_mistakes():
     st.subheader('general landing mistakes')
     mistakes = kb['general_landing_mistakes']
-    
+
     for mistake in mistakes.items():
         option = st.radio(mistake[0], ['none'] + mistake[1])
         if option == 'small':
@@ -149,7 +160,7 @@ def general_landing_mistakes():
 def general_mistakes():
     st.subheader('general mistakes')
     mistakes = kb['general_mistakes']
-    
+
     for mistake in mistakes.items():
         option = st.radio(mistake[0], ['none'] + mistake[1])
         if option == 'small':
@@ -170,7 +181,7 @@ def artistry():
 
     st.subheader('artistry')
     mistakes = kb['artistry']
-    
+
     for mistake in mistakes:
         option = st.checkbox(mistake)
         if option:
@@ -187,12 +198,13 @@ def combos():
 
     elems = st.session_state['selected_elements']
     st.subheader('element combinations')
-    
-    for elem_idx in range(len(elems) - 1):
-        if (elems[elem_idx][1]['element_type'], elems[elem_idx + 1][1]['element_type']) in[('dance', 'dance'), ('acro', 'acro'), ('acro', 'dance'), ('dance', 'acro')]:
-            option = st.radio(elems[elem_idx][0] + ' + ' + elems[elem_idx + 1][0] + '?', ['no', 'yes'])
-            if option == 'yes':
-                st.session_state['combo'] += 0.1
+
+    for elem in elems:
+        other_elems = [e for e in elems if e != elem]
+
+        option = st.radio(elem[0], ['none'] + [e[0] for e in other_elems])
+        if option != 'none':
+            st.session_state['combo'] += 0.1
 
     if st.button('Next'):
         st.session_state['state'] = 'results'
