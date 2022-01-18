163f231e2825489e096ceeaf0a5a2d28689d65ec
Paul Bricman <paulbricman@protonmail.com>
2022-01-18 13:20:48 +0200

    feat: a bunch of changes

--- components.py
+++ components.py
@@ -153,7 +153,7 @@ def detail_element(element):
                                               ][1]['info_landing_mistakes'] += [[mistake[0], option]]
 
     # Specific code for falls
-    check = cols[1].checkbox("Fall")
+    check = cols[1].checkbox("Fall", key=element[0])
     if check:
         nstd_check = cols[1].checkbox("Element not landed on feet first")
         if nstd_check:
@@ -185,8 +185,8 @@ def general_mistakes():
     st.session_state['general_mistakes'] = []
 
     for mistake in mistakes.items():
-        #option = st.radio(mistake[0], ['none'] + mistake[1])
-        #st.session_state['general_mistakes'] += [[mistake[0], option]]
+        # option = st.radio(mistake[0], ['none'] + mistake[1])
+        # st.session_state['general_mistakes'] += [[mistake[0], option]]
 
         # @paul can you check whether here the correct value is deducted?
         # As in: if the duration is longer than 1:20 it should deduct small and help from trainer
@@ -220,51 +220,57 @@ def artistry():
         st.experimental_rerun()
 
 
-def compute_skill_requirements():
+def compute_skill_requirements(all_CB):
     elems = st.session_state['selected_elements']
     srs = [None] * 4
 
     srs[0] = False
+    for combo in all_CB:
+        elem1 = get_element_by_name(combo[0])
+        elem2 = get_element_by_name(combo[1])
+        if elem1['element_type'] == ('dance' or 'jump') and elem2['element_type'] == ('dance' or 'jump'):
+            # The combo consists of
+            if elem1['element_type'] == 'jump'
+
     for elem in elems:
         # go through all elements
-        if elem[1]['element_type'] == ('dance' or 'jump'):
+        if elem[1]['info_combo'] != 'none' and elem[1]['element_type'] == ('dance' or 'jump') and elem[1]['valid'] == 1:
             # if element is a turn or a jump check if a combination is done
-            if elem[1]['info_combo'] != 'none':
-                # Combo found
-                flag = 0
-                for l in elem[1]['info_landing_mistakes']:
-                    # check whether this first element of the combination had a
-                    # landing mistake(the combo doesn't count then)
-                    if l[1] != 'none':
-                        # there is a landing mistake so combo doesnt count
-                        flag = 1
-                if flag == 0:
-                    # 1st element counts towards combo, now find 2nd element
-                    comboed_elem = [
-                        e for e in elems if e[0] == elem[1]['info_combo'] and e[1]['valid'] == 1][0]
-                    if comboed_elem[1]['element_type'] == ('dance' or 'jump'):
-                        # 2nd element follows dance requirement
-                        flag1 = 0
-                        for l in comboed_elem[1]['info_landing_mistakes']:
-                            # check whether this second element of the combination had a
-                            # landing mistake(the combo doesn't count then)
-                            if l[1] == 'very big':
-                                # there was a fall so combo doesnt count
-                                flag1 = 1
-                        if flag1 == 0:
-                            # 2nd element counts towards combo now check whether there was a jump at 180
-                            if elem[1]['element_type'] == ('Split jump' or 'Split leap'):
-                                for q in elem[1]['info_element_questions']:
-                                    if q[0] == 'What was the deviation from a 180º splits?' and q[1] == '0º':
-                                        srs[0] = True
-                                        break
-                            if comboed_elem[1]['element_type'] == ('Split jump' or 'Split leap'):
-                                for q in elem[1]['info_element_questions']:
-                                    if q[0] == 'What was the deviation from a 180º splits?' and q[1] == '0º':
-                                        srs[0] = True
-                                        break
+            # Combo found
+            flag = 0
+            for l in elem[1]['info_landing_mistakes']:
+                # check whether this first element of the combination had a
+                # landing mistake(the combo doesn't count then)
+                if l[1] != 'none':
+                    # there is a landing mistake so combo doesnt count
+                    flag = 1
+            if flag == 0:
+                # 1st element counts towards combo, now find 2nd element
+                comboed_elem = [
+                    e for e in elems if e[0] == elem[1]['info_combo'] and e[1]['valid'] == 1][0]
+                if comboed_elem[1]['element_type'] == ('dance' or 'jump'):
+                    # 2nd element follows dance requirement
+                    flag1 = 0
+                    for l in comboed_elem[1]['info_landing_mistakes']:
+                        # check whether this second element of the combination had a
+                        # landing mistake(the combo doesn't count then)
+                        if l[1] == 'very big':
+                            # there was a fall so combo doesnt count
+                            flag1 = 1
+                    if flag1 == 0:
+                        # 2nd element counts towards combo now check whether there was a jump at 180
+                        if elem[1]['element_type'] == ('Split jump' or 'Split leap'):
+                            for q in elem[1]['info_element_questions']:
+                                if q[0] == 'What was the deviation from a 180º splits?' and q[1] == '0º':
+                                    srs[0] = True
+                                    break
+                        if comboed_elem[1]['element_type'] == ('Split jump' or 'Split leap'):
+                            for q in elem[1]['info_element_questions']:
+                                if q[0] == 'What was the deviation from a 180º splits?' and q[1] == '0º':
+                                    srs[0] = True
+                                    break
 
-    srs[1] = len([e for e in elems if 'turn' in e[0]]) > 0
+    srs[1] = len([e for e in elems if 'Turn' in e[0]]) > 0
     srs[2] = len([e for e in elems if e[1]['element_type'] ==
                   'acro' and e[1]['difficulty'] in ['A', 'B']]) > 0
 
@@ -274,17 +280,18 @@ def compute_skill_requirements():
         if e[0] == 'Handstand':
             for q in e[1]["info_element_questions"]:
                 if q[0] == "Did the handstand reach vertical (90º)?":
-                    if q[1]["info_element_questions"][1] != "yes":
+                    if q[1] != "Yes":
                         relevant_elems.remove(e)
 
     srs[3] = len([e for e in relevant_elems]) > 0
 
-    return sum(srs) * 0.5, srs
+    return round(sum(srs) * 0.5, 2), srs
 
 
 def compute_combo_bonus():
     cb = 0
-    cbs = []
+    counting_cbs = []
+    all_cbs = []
 
     for elem in st.session_state['selected_elements']:
         if elem[1]['info_combo'] != 'none' and elem[1]['valid'] == 1:
@@ -303,7 +310,7 @@ def compute_combo_bonus():
                     e for e in st.session_state['selected_elements'] if e[0] == elem[1]['info_combo'] and e[1]['valid'] == 1][0]
 
                 # Check whether this combo was not already calculated or is a combination with itself
-                for comb in cbs:
+                for comb in all_cbs:
                     if (comb[0] == comboed_elem[0] and comb[1] == elem[0]) or (comboed_elem[0] == elem[0]):
                         flag = 1
 
@@ -317,14 +324,20 @@ def compute_combo_bonus():
 
                     if flag == 0:
                         # 2nd element counts towards combo now check element difficulty to find bonus
-                        if ['A', 'B'] == sorted([elem[1]['difficulty'], comboed_elem[1]['difficulty']]):
-                            cb += 0.2
-                            cbs += [[elem[0], comboed_elem[0]]]
-                        elif ['A', 'A'] == sorted([elem[1]['difficulty'], comboed_elem[1]['difficulty']]):
-                            cb += 0.1
-                            cbs += [[elem[0], comboed_elem[0]]]
+                        all_cbs += [[elem[0], comboed_elem[0]]]
 
-    return cb, cbs
+                        sorted_combo_elems = sorted(
+                            [elem[1]['difficulty'], comboed_elem[1]['difficulty']])
+                        if sorted_combo_elems == ['A', 'B']:
+                            cb += 0.2
+                            counting_cbs += [[[elem[0],
+                                               comboed_elem[0]], ['A', 'B']]]
+                        elif sorted_combo_elems == ['A', 'A']:
+                            cb += 0.1
+                            counting_cbs += [[[elem[0],
+                                               comboed_elem[0]], ['A', 'A']]]
+
+    return round(cb, 2), counting_cbs, all_cbs
 
 
 def compute_difficulty_score():
@@ -358,7 +371,7 @@ def compute_difficulty_score():
                 total_cnt += 1
                 counted_elems.append(elem)
 
-    return difficulty, counted_elems, counter
+    return round(difficulty, 2), counted_elems, counter
 
 
 def compute_artistry():
@@ -369,7 +382,7 @@ def compute_artistry():
         if mistake[1] == True:
             artistry -= 0.1
 
-    return artistry, [e[0] for e in mistakes if e[1] == True]
+    return round(artistry, 2), [e[0] for e in mistakes if e[1] == True]
 
 
 def compute_execution():
@@ -383,9 +396,9 @@ def compute_execution():
         elif mistake[1] == 'middle':
             execution -= 0.3
         elif mistake[1] == 'zero':
-            execution = 0
+            execution = 10  # positive number should not be accessible normally, this routine is not valid
 
-    if execution != 0:
+    if execution <= 0:
         for elem in st.session_state['selected_elements']:
             for answer in elem[1]['info_element_questions']:
                 if answer[0] == "What was the deviation from a 180º splits?":
@@ -433,7 +446,7 @@ def compute_execution():
             else:
                 execution -= landing_ex
 
-    return execution, [e[0] for e in general_mistakes if e[1] != 'none'], element_mistakes
+    return round(execution, 2), [e[0] for e in general_mistakes if e[1] != 'none'], element_mistakes
 
 
 def compute_n_score():
@@ -450,52 +463,70 @@ def compute_n_score():
         return 0
 
 
+def get_element_by_name(name):
+    return [e for e in st.session_state['selected_elements'] if e[0] == name]
+
+
 def results():
-    st.header('Results')
+    cols = st.columns(20)
+    with cols[0]:
+        st.header('Results')
 
-    st.subheader('D-Score')
-    diff, d_elems, counter = compute_difficulty_score()
-    print('DS', diff, d_elems, counter)
-    SR_score, SR = compute_skill_requirements()
-    CB_score, CB = compute_combo_bonus()
-    d_score = diff+SR_score+CB_score
+        st.subheader('D-Score')
+        diff, d_elems, counter = compute_difficulty_score()
+        CB_score, counting_CB, all_CB = compute_combo_bonus()
+        SR_score, SR = compute_skill_requirements(all_CB)
 
-    st.text("Difficulty ({}".format(counter[0]) + "TA + {}".format(counter[1]) + "A + {}".format(counter[2]) + "B)                                                                                +{}P. \n".format(diff) +
-            "Composition Requirements                                                                                  +{}P. \n".format(SR_score) +
-            "Connection Value                                                                                          +{}P. \n".format(CB_score) +
-            "--------------------------------------------------------------------------------------------------------------\n" +
-            "D-score                                                                                                   ={}P.".format(d_score))
+        d_score = round(diff+SR_score+CB_score, 2)
 
-    st.subheader(
-        'E-Score')
-    art_score, art_mistakes = compute_artistry()
-    ex_score, gen_mistakes, ex_mistakes = compute_execution()
-    n_score = compute_n_score()
-    e_score = 10.00+ex_score+art_score
+        st.text("Difficulty ({}".format(counter[0]) + "TA + {}".format(counter[1]) + "A + {}".format(counter[2]) + "B)                                                                                +{}P. \n".format(diff) +
+                "Composition Requirements                                                                                  +{}P. \n".format(SR_score) +
+                "Connection Value                                                                                          +{}P. \n".format(CB_score))
+        st.markdown('---')
+        st.text("D-score                                                                                                   ={}P.".format(d_score))
 
-    st.text("E-score                                                                                                   10.0P.\n" +
-            "Execution                                                           {}P.".format(ex_score) + "                    \n" +
-            "Artistry                                                            {}P.".format(art_score) + "                                {}P.\n".format(ex_score+art_score) +
-            "---------------------------------------------------------------------------------------------------------------\n" +
-            "E-score                                                                                                   ={}P.".format(e_score))
+        with st.expander('Difficulty Details'):
+            # [[elem name 1, A], [elem name 2, TA]]
+            for e in sorted(d_elems, key=lambda x: {'TA': 0, 'A': 1, 'B': 2}[x[1]]):
+                st.markdown('- ' + e[0] + ' **' + e[1] + '**')
 
-    st.metric(label="Final score", value=e_score+d_score)
-    if n_score != 0:
-        st.text("Final score after neutral deduction for short exercise applied \n" +
-                "{}P.".format(e_score+d_score) + " - {}P.(short exercise)".format(n_score) + " = {}P.".format(e_score + d_score - n_score))
-    # st.subheader('results')
-    # dscore = st.session_state['difficulty'] + \
-    #     st.session_state['combo'] + st.session_state['reqs']
-    # escore = 10 + st.session_state['execution'] + st.session_state['artistry']
+        with st.expander('Requirements Details'):
+            for e_idx, e in enumerate(SR):
+                st.markdown('- Requirement ' + str(e_idx + 1) +
+                            ': **' + str(e) + '**')
 
-    # results = pd.DataFrame([
-    #     ['difficulty score (DS)', st.session_state['difficulty']],
-    #     ['combo bonus (CB)', st.session_state['combo']],
-    #     ['skill requirements (SR)', st.session_state['reqs']],
-    #     ['execution', st.session_state['execution']],
-    #     ['artistry', st.session_state['artistry']],
-    #     ['D-score', dscore],
-    #     ['E-score', escore],
-    # ], columns=['rubric', 'score'])
+        with st.expander('Connection Details'):
+            for e_idx, e in enumerate(counting_CB):
+                st.markdown(str(e_idx + 1) + '.' +
+                            ': **' + e[0] + ' + ' + e[1] + '**')
 
-    # st.dataframe(results)
+        st.subheader(
+            'E-Score')
+        art_score, art_mistakes = compute_artistry()
+        ex_score, gen_mistakes, ex_mistakes = compute_execution()
+        n_score = compute_n_score()
+        e_score = round(10.00+ex_score+art_score, 2)
+
+        st.text("E-score                                                                                                   10.0P.\n" +
+                "Execution                                                           {}P.".format(ex_score) + "                    \n" +
+                "Artistry                                                            {}P.".format(art_score) + "                                {}P.\n".format(ex_score+art_score))
+        st.markdown('---')
+        st.text("E-score                                                                                                   ={}P.".format(e_score))
+
+        with st.expander('Execution Details'):
+            st.markdown('#### General mistakes')
+            for e_idx, e in enumerate(gen_mistakes):
+                st.markdown(str(e_idx + 1) + '. ' + e)
+
+            st.markdown('#### Execution mistakes')
+            for e_idx, e in enumerate(ex_mistakes):
+                st.markdown(str(e_idx + 1) + '. ' + e)
+
+        with st.expander('Artistry Details'):
+            for e_idx, e in enumerate(art_mistakes):
+                st.markdown(str(e_idx + 1) + '. ' + e)
+
+        st.metric(label="Final score", value=e_score+d_score)
+        if n_score != 0:
+            st.text("Final score after neutral deduction for short exercise applied \n" +
+                    "{}P.".format(e_score+d_score) + " - {}P.(short exercise)".format(n_score) + " = {}P.".format(round(e_score + d_score - n_score, 2)))
