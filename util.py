2124446de65ec7da1ca2b38c935d7cfa93ab92bb
Paul Bricman <paulbricman@protonmail.com>
2022-01-19 06:47:50 +0200

    feat: jury sign error

--- util.py
+++ util.py
@@ -14,9 +14,22 @@ def init_session_state():
     st.session_state['big_mistakes'] = 0
     st.session_state['falls'] = 0
     st.session_state['connections'] = 0
-    
+
     if 'state' not in st.session_state.keys():
         st.session_state['state'] = 'choose_elements'
     if 'current_element' not in st.session_state.keys():
         st.session_state['current_element'] = 0
-    
+
+
+def hard_reset_session_state():
+    st.session_state['small_mistakes'] = 0
+    st.session_state['big_mistakes'] = 0
+    st.session_state['falls'] = 0
+    st.session_state['connections'] = 0
+
+    st.session_state['state'] = 'choose_elements'
+    st.session_state['current_element'] = 0
+    st.session_state['selected_elements'] = []
+    st.session_state['general_mistakes'] = []
+    st.session_state['artistry_mistakes'] = []
+    st.experimental_rerun()
